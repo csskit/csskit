960996b17342f9b2aa3c50278496b7061c7a1679
Keith Cirkel <keithamus@users.noreply.github.com>
2025-06-30 22:27:04 +0100

    add easing_function support (#114)
    Implements the easing functions, such as `linear()`, and uncomments the
    `animation-timing-function`, & `transition-timing-function` style values
    as they can now be parsed with this addition.
    
    This required some improvements to def.rs which ensures that lifetimes
    are added to data structures that require them, even inside of a `Vec`.

--- crates/css_ast/src/types/easing_function.rs
+++ crates/css_ast/src/types/easing_function.rs
@@ -1,29 +1,239 @@
+use bumpalo::collections::Vec;
 use css_lexer::{Cursor, SourceOffset};
-use css_parse::{CursorSink, Parse, Parser, Peek, Result as ParserResult, ToCursors, T};
+use css_parse::{
+	diagnostics, function_set, keyword_set, Build, CursorSink, Parse, Parser, Peek, Result as ParserResult, ToCursors,
+	T,
+};
 
+use crate::CSSInt;
+
+function_set!(EasingFunctionKeyword { Linear: "linear", CubicBezier: "cubic-bezier", Steps: "steps" });
+
+keyword_set!(EasingKeyword {
+	Linear: "linear",
+	Ease: "ease",
+	EaseIn: "ease-in",
+	EaseOut: "ease-out",
+	EaseInOut: "ease-in-out",
+	StepStart: "step-start",
+	StepEnd: "step-end",
+});
+
+keyword_set!(StepPosition {
+	JumpStart: "jump-start",
+	JumpEnd: "jump-end",
+	JumpNone: "jump-none",
+	JumpBoth: "jump-both",
+	Start: "start",
+	End: "end",
+});
+
+// https://drafts.csswg.org/css-easing-2/#typedef-easing-function
+// <easing-function> = <linear-easing-function>
+//                      | <cubic-bezier-easing-function>
+//                      | <step-easing-function>
+//
+// <linear-easing-function> = linear | <linear()>
+//
+// linear() = linear( [ <number> && <percentage>{0,2} ]# )
+//
+// <cubic-bezier-easing-function> =
+// 	ease | ease-in | ease-out | ease-in-out | <cubic-bezier()>
+//
+// cubic-bezier() = cubic-bezier( [ <number [0,1]>, <number> ]#{2} )
+//
+// <step-easing-function> = step-start | step-end | <steps()>
+//
+// steps() = steps( <integer>, <step-position>?)
+//
+// <step-position> = jump-start | jump-end | jump-none | jump-both | start | end
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize), serde())]
-pub struct EasingFunction;
+pub enum EasingFunction<'a> {
+	Linear(T![Ident]),
+	Ease(T![Ident]),
+	EaseIn(T![Ident]),
+	EaseOut(T![Ident]),
+	EaseInOut(T![Ident]),
+	StepStart(T![Ident]),
+	StepEnd(T![Ident]),
+	LinearFunction(T![Function], Vec<'a, (T![Number], Option<T![Dimension::%]>, Option<T![Dimension::%]>, Option<T![,]>)>, Option<T![')']>),
+	CubicBezierFunction(
+		T![Function],
+		T![Number],
+		Option<T![,]>,
+		T![Number],
+		Option<T![,]>,
+		T![Number],
+		Option<T![,]>,
+		T![Number],
+		Option<T![')']>,
+	),
+	StepFunction(T![Function], CSSInt, Option<T![,]>, Option<StepPosition>, Option<T![')']>),
+}
 
-impl<'a> Peek<'a> for EasingFunction {
+impl<'a> Peek<'a> for EasingFunction<'a> {
 	fn peek(p: &Parser<'a>, c: Cursor) -> bool {
-		todo!();
+		EasingKeyword::peek(p, c) || EasingFunctionKeyword::peek(p, c)
 	}
 }
 
-impl<'a> Parse<'a> for EasingFunction {
+impl<'a> Parse<'a> for EasingFunction<'a> {
 	fn parse(p: &mut Parser<'a>) -> ParserResult<Self> {
-		todo!();
+		if p.peek::<EasingKeyword>() {
+			let keyword = p.parse::<EasingKeyword>()?;
+			let c = keyword.into();
+			let ident = <T![Ident]>::build(p, c);
+			return match keyword {
+				EasingKeyword::Linear(c) => Ok(Self::Linear(ident)),
+				EasingKeyword::Ease(c) => Ok(Self::Ease(ident)),
+				EasingKeyword::EaseIn(c) => Ok(Self::EaseIn(ident)),
+				EasingKeyword::EaseOut(c) => Ok(Self::EaseOut(ident)),
+				EasingKeyword::EaseInOut(c) => Ok(Self::EaseInOut(ident)),
+				EasingKeyword::StepStart(c) => Ok(Self::StepStart(ident)),
+				EasingKeyword::StepEnd(c) => Ok(Self::StepEnd(ident)),
+			};
+		}
+		let keyword = p.parse::<EasingFunctionKeyword>()?;
+		let c = keyword.into();
+		let function = <T![Function]>::build(p, c);
+		match keyword {
+			EasingFunctionKeyword::Linear(f) => {
+				let mut stops = Vec::new_in(p.bump());
+				loop {
+					if p.at_end() || p.peek::<T![')']>() {
+						break;
+					}
+					let c = p.peek_n(1);
+					let mut num = p.parse_if_peek::<T![Number]>()?;
+					let percent = p.parse_if_peek::<T![Dimension::%]>()?;
+					let percent2 = p.parse_if_peek::<T![Dimension::%]>()?;
+					if num.is_none() {
+						num = Some(p.parse::<T![Number]>()?);
+					}
+					stops.push((num.unwrap(), percent, percent2, p.parse_if_peek::<T![,]>()?));
+				}
+				if stops.len() < 2 {
+					Err(diagnostics::NotEnoughArguments("linear".into(), 2, stops.len(), f.into()))?
+				}
+				Ok(Self::LinearFunction(function, stops, p.parse_if_peek::<T![')']>()?))
+			}
+			EasingFunctionKeyword::CubicBezier(_) => {
+				let x1 = p.parse::<T![Number]>()?;
+				let c1 = p.parse_if_peek::<T![,]>()?;
+				let x2 = p.parse::<T![Number]>()?;
+				let c2 = p.parse_if_peek::<T![,]>()?;
+				let y1 = p.parse::<T![Number]>()?;
+				let c3 = p.parse_if_peek::<T![,]>()?;
+				let y2 = p.parse::<T![Number]>()?;
+				Ok(Self::CubicBezierFunction(function, x1, c1, x2, c2, y1, c3, y2, p.parse_if_peek::<T![')']>()?))
+			}
+			EasingFunctionKeyword::Steps(_) => {
+				let number = p.parse::<CSSInt>()?;
+				let comma = p.parse_if_peek::<T![,]>()?;
+				let position = p.parse_if_peek::<StepPosition>()?;
+				dbg!(number, position);
+				Ok(Self::StepFunction(function, number, comma, position, p.parse_if_peek::<T![')']>()?))
+			}
+		}
 	}
 }
 
-impl<'a> ToCursors for EasingFunction {
+impl<'a> ToCursors for EasingFunction<'a> {
 	fn to_cursors(&self, s: &mut impl CursorSink) {
-		todo!();
+		match self {
+			Self::Linear(c) => s.append(c.into()),
+			Self::Ease(c) => s.append(c.into()),
+			Self::EaseIn(c) => s.append(c.into()),
+			Self::EaseOut(c) => s.append(c.into()),
+			Self::EaseInOut(c) => s.append(c.into()),
+			Self::StepStart(c) => s.append(c.into()),
+			Self::StepEnd(c) => s.append(c.into()),
+			Self::LinearFunction(f, stops, paren) => {
+				s.append(f.into());
+				for (number, percent, percent2, comma) in stops {
+					s.append(number.into());
+					if let Some(percent) = percent {
+						s.append(percent.into());
+					}
+					if let Some(percent) = percent2 {
+						s.append(percent.into());
+					}
+					if let Some(comma) = comma {
+						s.append(comma.into());
+					}
+				}
+				if let Some(paren) = paren {
+					s.append(paren.into());
+				}
+			}
+			Self::CubicBezierFunction(f, x1, c1, x2, c2, y1, c3, y2, paren) => {
+				s.append(f.into());
+				s.append(x1.into());
+				if let Some(comma) = c1 {
+					s.append(comma.into());
+				}
+				s.append(x2.into());
+				if let Some(comma) = c2 {
+					s.append(comma.into());
+				}
+				s.append(y1.into());
+				if let Some(comma) = c3 {
+					s.append(comma.into());
+				}
+				s.append(y2.into());
+				if let Some(paren) = paren {
+					s.append(paren.into());
+				}
+			}
+			Self::StepFunction(f, int, comma, pos, paren) => {
+				s.append(f.into());
+				s.append((*int).into());
+				if let Some(comma) = comma {
+					s.append(comma.into());
+				}
+				if let Some(pos) = pos {
+					s.append(pos.into());
+				}
+				if let Some(paren) = paren {
+					s.append(paren.into());
+				}
+			}
+		}
 	}
 }
 
 #[cfg(test)]
 mod tests {
 	use super::*;
+	use css_parse::{assert_parse, assert_parse_error};
+
+	#[test]
+	fn size_test() {
+		assert_eq!(std::mem::size_of::<EasingFunction>(), 128);
+	}
+
+	#[test]
+	fn test_writes() {
+		assert_parse!(EasingFunction, "ease-in-out");
+		assert_parse!(EasingFunction, "linear(0,1)");
+		assert_parse!(EasingFunction, "linear(0,0.25,1)");
+		assert_parse!(EasingFunction, "linear(0,0.5 25% 75%,1)");
+		assert_parse!(EasingFunction, "cubic-bezier(0.25,0.1,0.25,1)");
+		assert_parse!(EasingFunction, "cubic-bezier(0.1,-0.6,0.2,0)");
+		assert_parse!(EasingFunction, "cubic-bezier(0,0,1,1)");
+		assert_parse!(EasingFunction, "steps(4,end)");
+		assert_parse!(EasingFunction, "steps(10,jump-both)");
+		// // Incomplete but recoverable
+		assert_parse!(EasingFunction, "linear(0,0.25,1");
+		assert_parse!(EasingFunction, "linear(0 0.25 1)");
+		assert_parse!(EasingFunction, "cubic-bezier(0.1 -0.6 0.2 0)");
+	}
+
+	#[test]
+	fn test_errors() {
+		assert_parse_error!(EasingFunction, "foo");
+		assert_parse_error!(EasingFunction, "linear()");
+		assert_parse_error!(EasingFunction, "cubic-bezier(0.1, red, 1.0, green)");
+	}
 }
