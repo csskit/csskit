d7cc27462c8fcd8487af64c097c47ca426e8864a
Marais Rossouw <me@marais.co>
2025-07-07 12:19:21 +1000

    feat: implements `skew*()`

--- crates/css_ast/src/types/transform_skew_y.rs
+++ crates/css_ast/src/types/transform_skew_y.rs
@@ -1,59 +1,43 @@
 #![allow(warnings)]
+use crate::types::SkewKind;
 use crate::units::{Angle, CSSInt};
 use css_lexer::{Cursor, SourceOffset};
 use css_parse::{CursorSink, Parse, Parser, Peek, Result as ParserResult, T, ToCursors, diagnostics, keyword_set};
 use csskit_derives::{Parse, Peek, ToCursors};
 
-#[derive(Parse, Peek, ToCursors, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize), serde())]
-pub enum SkewKind {
-	Angle(Angle),
-	Zero(T![Number]),
-}
-
-// https://drafts.csswg.org/css-transforms-1/#funcdef-transform-skew
-// skew() = skew( [ <angle> | <zero> ] , [ <angle> | <zero> ]? )
+// https://drafts.csswg.org/css-transforms-1/#funcdef-transform-skewx
+// skewY() = skewY( [ <angle> | <zero> ] )
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize), serde())]
-pub struct Skew {
+pub struct SkewY {
 	skew: T![Function],
 	pub x: SkewKind,
-	x_: Option<T![,]>,
-	pub y: Option<SkewKind>,
 	close: Option<T![')']>,
 }
 
-impl<'a> Peek<'a> for Skew {
+impl<'a> Peek<'a> for SkewY {
 	fn peek(p: &Parser<'a>, c: Cursor) -> bool {
-		<T![Function]>::peek(p, c) && p.eq_ignore_ascii_case(c, "skew")
+		<T![Function]>::peek(p, c) && p.eq_ignore_ascii_case(c, "skewy")
 	}
 }
 
-impl<'a> Parse<'a> for Skew {
+impl<'a> Parse<'a> for SkewY {
 	fn parse(p: &mut Parser<'a>) -> ParserResult<Self> {
 		let skew = p.parse::<T![Function]>()?;
 		let c: Cursor = skew.into();
-		if !p.eq_ignore_ascii_case(c, "skew") {
+		if !p.eq_ignore_ascii_case(c, "skewy") {
 			Err(diagnostics::UnexpectedFunction(p.parse_str(c).into(), c.into()))?
 		}
 		let x = p.parse::<SkewKind>()?;
-		let x_ = p.parse_if_peek::<T![,]>()?;
-		let y = p.parse_if_peek::<SkewKind>()?;
 		let close = p.parse_if_peek::<T![')']>()?;
-		Ok(Self { skew, x, x_, y, close })
+		Ok(Self { skew, x, close })
 	}
 }
 
-impl<'a> ToCursors for Skew {
+impl<'a> ToCursors for SkewY {
 	fn to_cursors(&self, s: &mut impl CursorSink) {
 		ToCursors::to_cursors(&self.skew, s);
 		ToCursors::to_cursors(&self.x, s);
-		if let Some(ref x_) = self.x_ {
-			ToCursors::to_cursors(x_, s);
-		}
-		if let Some(ref y) = self.y {
-			ToCursors::to_cursors(y, s);
-		}
 		if let Some(ref close) = self.close {
 			ToCursors::to_cursors(close, s);
 		}
@@ -67,19 +51,18 @@ mod tests {
 
 	#[test]
 	fn size_test() {
-		assert_eq!(std::mem::size_of::<Skew>(), 76);
+		assert_eq!(std::mem::size_of::<SkewY>(), 44);
 	}
 
 	#[test]
 	fn test_writes() {
-		assert_parse!(Skew, "skew(1deg,2deg)");
-		assert_parse!(Skew, "skew(0,0)");
-		assert_parse!(Skew, "skew(1deg)");
+		assert_parse!(SkewY, "skewY(1deg");
+		assert_parse!(SkewY, "skewY(0)");
 	}
 
 	#[test]
 	fn test_errors() {
-		assert_parse_error!(Skew, "skew()");
-		assert_parse_error!(Skew, "skew(foo)");
+		assert_parse_error!(SkewY, "skewY()");
+		assert_parse_error!(SkewY, "skewY(foo)");
 	}
 }
