e4bf1a3770a7c5952a2e212a2bc1c30c822e2e63
Marais Rossouw <me@marais.co>
2025-07-07 18:39:21 +1000

    (css_ast): Enable `<shadow>`

--- crates/css_ast/src/types/shadow.rs
+++ crates/css_ast/src/types/shadow.rs
@@ -1,32 +1,121 @@
 #![allow(warnings)]
+use std::ops::Deref;
+
+use crate::types::Color;
+use crate::units::{Length, Unit};
 use css_lexer::{Cursor, SourceOffset};
-use css_parse::{CursorSink, Parse, Parser, Peek, Result as ParserResult, T, ToCursors};
+use css_parse::{CursorSink, Parse, Parser, Peek, Result as ParserResult, T, ToCursors, diagnostics};
 
 // https://drafts.csswg.org/css-backgrounds-3/#typedef-shadow
 // <shadow> = <color>? && [<length>{2} <length [0,∞]>? <length>?] && inset?
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize), serde())]
-pub struct Shadow;
+pub struct Shadow {
+	pub color: Option<Color>,
+	pub offset: (Length, Length),
+	pub blur_radius: Option<Length>,
+	pub spread_radius: Option<Length>,
+	pub inset: Option<T![Ident]>,
+}
 
 impl<'a> Peek<'a> for Shadow {
 	fn peek(p: &Parser<'a>, c: Cursor) -> bool {
-		todo!();
+		Color::peek(p, c) || Length::peek(p, c)
 	}
 }
 
 impl<'a> Parse<'a> for Shadow {
 	fn parse(p: &mut Parser<'a>) -> ParserResult<Self> {
-		todo!();
+		let color = p.parse_if_peek::<Color>()?;
+
+		let x = p.parse::<Length>()?;
+		let y = p.parse::<Length>()?;
+
+		let blur_radius = p.parse_if_peek::<Length>()?;
+		if let Some(blur) = blur_radius {
+			let c: Cursor = blur.into();
+			if blur.is_negative() {
+				Err(diagnostics::NumberTooSmall(blur.into(), c.into()))?
+			}
+		}
+
+		let spread_radius = p.parse_if_peek::<Length>()?;
+
+		let inset = p.parse_if_peek::<T![Ident]>()?;
+		if let Some(ident) = inset {
+			if !p.eq_ignore_ascii_case(ident.into(), "inset") {
+				let c: Cursor = x.into();
+				Err(diagnostics::UnexpectedIdent(p.parse_str(c).into(), c.into()))?
+			}
+		}
+
+		Ok(Self { color, offset: (x, y), blur_radius, spread_radius, inset })
 	}
 }
 
 impl<'a> ToCursors for Shadow {
 	fn to_cursors(&self, s: &mut impl CursorSink) {
-		todo!();
+		if let Some(ref color) = self.color {
+			ToCursors::to_cursors(color, s);
+		}
+		let (ref x, ref y) = self.offset;
+		ToCursors::to_cursors(x, s);
+		ToCursors::to_cursors(y, s);
+		if let Some(ref blur) = self.blur_radius {
+			ToCursors::to_cursors(blur, s);
+		}
+		if let Some(ref spread) = self.spread_radius {
+			ToCursors::to_cursors(spread, s);
+		}
+		if let Some(ref inset) = self.inset {
+			ToCursors::to_cursors(inset, s);
+		}
 	}
 }
 
 #[cfg(test)]
 mod tests {
 	use super::*;
+	use css_parse::{assert_parse, assert_parse_error};
+
+	#[test]
+	fn size_test() {
+		assert_eq!(std::mem::size_of::<Shadow>(), 240);
+	}
+
+	#[test]
+	fn test_writes() {
+		assert_parse!(Shadow, "10px 20px");
+		assert_parse!(Shadow, "10px 20px 5px");
+		assert_parse!(Shadow, "10px 20px 5px 3px");
+		assert_parse!(Shadow, "red 10px 20px");
+		assert_parse!(Shadow, "#ff0000 10px 20px 5px");
+		assert_parse!(Shadow, "rgba(255,0,0,0.5)10px 20px 5px 3px");
+		assert_parse!(Shadow, "10px 20px inset");
+		assert_parse!(Shadow, "10px 20px 5px inset");
+		assert_parse!(Shadow, "10px 20px 5px 3px inset");
+		assert_parse!(Shadow, "red 10px 20px inset");
+		assert_parse!(Shadow, "blue 10px 20px 5px 3px inset");
+		assert_parse!(Shadow, "-10px -20px");
+		assert_parse!(Shadow, "red -10px -20px 5px");
+		assert_parse!(Shadow, "0 0");
+		assert_parse!(Shadow, "0 0 0");
+		assert_parse!(Shadow, "0 0 0 0");
+		assert_parse!(Shadow, "1em 2em");
+		assert_parse!(Shadow, "1rem 2rem 0.5rem");
+	}
+
+	#[test]
+	fn test_errors() {
+		assert_parse_error!(Shadow, "");
+		assert_parse_error!(Shadow, "10% 20%");
+		assert_parse_error!(Shadow, "10px");
+		assert_parse_error!(Shadow, "red");
+		assert_parse_error!(Shadow, "inset");
+		assert_parse_error!(Shadow, "10px 20px -5px");
+		assert_parse_error!(Shadow, "10px 20px 5px 3px 7px");
+		assert_parse_error!(Shadow, "10px 20px notinset");
+		assert_parse_error!(Shadow, "10px 20px 5px inset 3px");
+		assert_parse_error!(Shadow, "10px 20px 5px 3px inset extra");
+	}
 }
