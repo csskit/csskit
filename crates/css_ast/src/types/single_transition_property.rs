173ec8a8196a5600e6b873ca48699deb25a0a5b3
Marais Rossouw <me@marais.co>
2025-07-09 17:57:58 +1000

    feat: implements `single-transition-property`

--- crates/css_ast/src/types/single_transition_property.rs
+++ crates/css_ast/src/types/single_transition_property.rs
@@ -1,32 +1,46 @@
 #![allow(warnings)]
 use css_lexer::{Cursor, SourceOffset};
 use css_parse::{CursorSink, Parse, Parser, Peek, Result as ParserResult, T, ToCursors};
+use csskit_derives::{Peek, ToCursors};
 
 // https://drafts.csswg.org/css-transitions-1/#single-transition-property
 // <single-transition-property> = all | <custom-ident>
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(ToCursors, Peek, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize), serde())]
-pub struct SingleTransitionProperty;
-
-impl<'a> Peek<'a> for SingleTransitionProperty {
-	fn peek(p: &Parser<'a>, c: Cursor) -> bool {
-		todo!();
-	}
+pub enum SingleTransitionProperty {
+	All(T![Ident]),
+	CustomIdent(T![Ident]),
 }
 
 impl<'a> Parse<'a> for SingleTransitionProperty {
 	fn parse(p: &mut Parser<'a>) -> ParserResult<Self> {
-		todo!();
-	}
-}
+		let ident = p.parse::<T![Ident]>()?;
+		if p.eq_ignore_ascii_case(ident.into(), "all") {
+			return Ok(SingleTransitionProperty::All(ident));
+		}
 
-impl<'a> ToCursors for SingleTransitionProperty {
-	fn to_cursors(&self, s: &mut impl CursorSink) {
-		todo!();
+		Ok(SingleTransitionProperty::CustomIdent(ident))
 	}
 }
 
 #[cfg(test)]
 mod tests {
 	use super::*;
+	use css_parse::{assert_parse, assert_parse_error};
+
+	#[test]
+	fn size_test() {
+		assert_eq!(std::mem::size_of::<SingleTransitionProperty>(), 16);
+	}
+
+	#[test]
+	fn test_writes() {
+		assert_parse!(SingleTransitionProperty, "all");
+		assert_parse!(SingleTransitionProperty, "bar");
+	}
+
+	#[test]
+	fn test_errors() {
+		assert_parse_error!(SingleTransitionProperty, "123deg");
+	}
 }
