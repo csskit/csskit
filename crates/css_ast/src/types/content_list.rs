3818648b91929a34a41bad9d3f54e9461fb64efe
Marais Rossouw <me@marais.co>
2025-07-10 14:49:57 +1000

    feat: adds `string()`

--- crates/css_ast/src/types/content_list.rs
+++ crates/css_ast/src/types/content_list.rs
@@ -1,6 +1,7 @@
 #![allow(warnings)]
 use bumpalo::collections::Vec;
-use css_parse::{Parse, Parser, Result as ParserResult, T, keyword_set};
+use css_lexer::Cursor;
+use css_parse::{Build, Parse, Parser, Peek, Result as ParserResult, T, diagnostics, function_set, keyword_set};
 use csskit_derives::{IntoSpan, Parse, Peek, ToCursors};
 
 use crate::types::{Image, Quote};
@@ -23,6 +24,7 @@ type AttrFunction = crate::Todo;
 pub struct ContentList<'a>(pub Vec<'a, ContentListItem<'a>>);
 
 keyword_set!(ContentsKeyword, "contents");
+function_set!(ContentListFunctionKeyword { String: "string" });
 
 #[derive(IntoSpan, ToCursors, Parse, Peek, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize), serde())]
@@ -34,7 +36,7 @@ pub enum ContentListItem<'a> {
 	Quote(Quote),
 	LeaderFunction(LeaderFunction),
 	Target(Target),
-	StringFunction(T![Function]),
+	StringFunction(StringFunction),
 	ContentFunction(ContentFunction),
 	Counter(Counter),
 }
@@ -42,6 +44,45 @@ pub enum ContentListItem<'a> {
 impl<'a> Parse<'a> for ContentList<'a> {
 	fn parse(_p: &mut Parser<'a>) -> ParserResult<Self> {
 		todo!()
+	}
+}
+
+/// string()
+
+keyword_set!(NamePresencece { First: "first", Start: "start", Last: "last", FirstExcept: "first-except" });
+
+// https://drafts.csswg.org/css-content-3/#string-function
+// string() = string( <custom-ident> , [ first | start | last | first-except ]? )
+#[derive(IntoSpan, ToCursors, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize), serde())]
+pub struct StringFunction(
+	pub T![Function],
+	pub T![Ident],
+	pub Option<T![,]>,
+	pub Option<NamePresencece>,
+	pub Option<T![')']>,
+);
+
+impl<'a> Peek<'a> for StringFunction {
+	fn peek(p: &Parser<'a>, c: Cursor) -> bool {
+		// TODO? is this string eq needed?
+		ContentListFunctionKeyword::peek(p, c) && p.eq_ignore_ascii_case(c, "string")
+	}
+}
+
+impl<'a> Parse<'a> for StringFunction {
+	fn parse(p: &mut Parser<'a>) -> ParserResult<Self> {
+		let func = p.parse::<ContentListFunctionKeyword>()?;
+		if let ContentListFunctionKeyword::String(cursor) = func {
+			let ident = p.parse::<T![Ident]>()?;
+			let comma = p.parse_if_peek::<T![,]>()?;
+			let presence = p.parse_if_peek::<NamePresencece>()?;
+			let close = p.parse_if_peek::<T![')']>()?;
+			return Ok(StringFunction(<T![Function]>::build(p, cursor), ident, comma, presence, close));
+		};
+
+		let c: Cursor = func.into();
+		Err(diagnostics::UnexpectedFunction(func.into(), c.into()))?
 	}
 }
 
@@ -57,11 +98,14 @@ mod tests {
 
 	#[test]
 	fn test_writes() {
-		// assert_parse!(ContentList, "contents");
+		assert_parse!(StringFunction, "string(heading)");
+		assert_parse!(StringFunction, "string(heading,first)");
+		assert_parse!(StringFunction, "string(heading,first)");
 	}
 
 	#[test]
 	fn test_errors() {
-		// assert_parse_error!(ContentList, "foo");
+		assert_parse_error!(StringFunction, "string()");
+		assert_parse_error!(StringFunction, "foo()");
 	}
 }
