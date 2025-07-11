2bc7152ca1a94ad4127cee28d99db090a5bf5157
Marais Rossouw <me@marais.co>
2025-07-11 16:20:19 +1000

    chore: gets `Span` in

--- crates/css_ast/src/types/grid_line.rs
+++ crates/css_ast/src/types/grid_line.rs
@@ -1,7 +1,9 @@
 #![allow(warnings)]
 use css_lexer::{Cursor, SourceOffset};
-use css_parse::{CursorSink, Parse, Parser, Peek, Result as ParserResult, T, ToCursors, keyword_set};
+use css_parse::{CursorSink, Parse, Parser, Peek, Result as ParserResult, T, ToCursors, diagnostics, keyword_set};
 use csskit_derives::{IntoSpan, Parse, Peek, ToCursors};
+
+use crate::Unit;
 
 keyword_set!(GridLineKeywords { Auto: "auto", Span: "span" });
 
@@ -11,6 +13,7 @@ keyword_set!(GridLineKeywords { Auto: "auto", Span: "span" });
 #[cfg_attr(feature = "serde", derive(serde::Serialize), serde())]
 pub enum GridLine {
 	Auto(GridLineKeywords),
+	Span(GridLineKeywords, T![Number], T![Ident]),
 }
 
 impl<'a> Parse<'a> for GridLine {
@@ -18,7 +21,48 @@ impl<'a> Parse<'a> for GridLine {
 		if let Some(keyword) = p.parse_if_peek::<GridLineKeywords>()? {
 			match keyword {
 				GridLineKeywords::Auto(_) => return Ok(GridLine::Auto(keyword)),
-				_ => todo!(),
+				GridLineKeywords::Span(c) => {
+					let mut num = None;
+					let mut ident = None;
+
+					while num.is_none() || ident.is_none() {
+						if num.is_none() {
+							num = p.parse_if_peek::<T![Number]>()?;
+							if num.is_some() {
+								continue;
+							}
+						}
+
+						if ident.is_none() {
+							ident = p.parse_if_peek::<T![Ident]>()?;
+							if ident.is_some() {
+								continue;
+							}
+						}
+
+						break;
+					}
+
+					if num.is_none() && ident.is_none() {
+						let c: Cursor = p.parse::<T![Any]>()?.into();
+						Err(diagnostics::Unexpected(c.into(), c.into()))?
+					}
+
+					let num = num.unwrap();
+					let ident = ident.unwrap();
+
+					if !num.is_int() {
+						let c: Cursor = num.into();
+						Err(diagnostics::ExpectedInt(num.into(), c.into()))?
+					}
+
+					if !(num.is_positive() && num.value() != 0.0) {
+						let c: Cursor = num.into();
+						Err(diagnostics::NumberTooSmall(num.into(), c.into()))?
+					}
+
+					return Ok(Self::Span(keyword, num, ident));
+				}
 			}
 		}
 
@@ -39,5 +83,14 @@ mod tests {
 	#[test]
 	fn test_writes() {
 		assert_parse!(GridLine, "auto");
+		assert_parse!(GridLine, "span 1 foo");
+		assert_parse!(GridLine, "span foo 1", "span 1 foo");
+	}
+
+	#[test]
+	fn test_errors() {
+		assert_parse_error!(GridLine, "span 0 foo");
+		assert_parse_error!(GridLine, "span 1.2 foo");
+		assert_parse_error!(GridLine, "span -2 foo");
 	}
 }
