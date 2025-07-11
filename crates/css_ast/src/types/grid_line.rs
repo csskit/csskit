4c7fe474beaeead38497fb289c830e888364d8e6
Marais Rossouw <me@marais.co>
2025-07-11 16:00:33 +1000

    chore: scaffolds GridLine

--- crates/css_ast/src/types/grid_line.rs
+++ crates/css_ast/src/types/grid_line.rs
@@ -1,14 +1,43 @@
 #![allow(warnings)]
 use css_lexer::{Cursor, SourceOffset};
-use css_parse::{CursorSink, Parse, Parser, Peek, Result as ParserResult, T, ToCursors};
+use css_parse::{CursorSink, Parse, Parser, Peek, Result as ParserResult, T, ToCursors, keyword_set};
+use csskit_derives::{IntoSpan, Parse, Peek, ToCursors};
 
-use crate::Todo;
+keyword_set!(GridLineKeywords { Auto: "auto", Span: "span" });
 
 // https://drafts.csswg.org/css-grid-2/#typedef-grid-row-start-grid-line
 // <grid-line> = auto | <custom-ident> | [ [ <integer [-∞,-1]> | <integer [1,∞]> ] && <custom-ident>? ] | [ span && [ <integer [1,∞]> || <custom-ident> ] ]
-pub type GridLine = Todo;
+#[derive(IntoSpan, ToCursors, Peek, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize), serde())]
+pub enum GridLine {
+	Auto(GridLineKeywords),
+}
+
+impl<'a> Parse<'a> for GridLine {
+	fn parse(p: &mut Parser<'a>) -> ParserResult<Self> {
+		if let Some(keyword) = p.parse_if_peek::<GridLineKeywords>()? {
+			match keyword {
+				GridLineKeywords::Auto(_) => return Ok(GridLine::Auto(keyword)),
+				_ => todo!(),
+			}
+		}
+
+		todo!()
+	}
+}
 
 #[cfg(test)]
 mod tests {
 	use super::*;
+	use css_parse::{assert_parse, assert_parse_error};
+
+	#[test]
+	fn size_test() {
+		// assert_eq!(std::mem::size_of::<GridLine>(), 1);
+	}
+
+	#[test]
+	fn test_writes() {
+		assert_parse!(GridLine, "auto");
+	}
 }
