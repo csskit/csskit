370266f1ff9fef4c914076e6de9b10128886473b
Keith Cirkel <keithamus@users.noreply.github.com>
2025-07-12 09:42:21 +0100

    uncomment and clean up Position tests  (#183)
    I noticed a bunch of `position` tests were left commented out. So I went
    to uncomment them, but I couldn't resist building on top of @maraisr's
    PR #184 which allows matching on the AST nodes, which is especially
    helpful for a complex type like `Position`. I added some small
    refinements on top (such as nicer error messages):
    
    <img width="632" height="407" alt="Image"
    src="https://github.com/user-attachments/assets/8fdb2873-fea4-42c0-9b17-955ac1d32446"
    />
    
    I also realised we're not really excercising or testing spans, or
    testing cases where the parser doesn't entirely parse, so I figured I'd
    also introduce `assert_parse_span`. This lets you write something nice
    and readable like:
    
    ```rs
    assert_parse_span!(
    	Position,
    	r#"
    	right var(--foo)
    	^^^^^
    "#
    );
    ```
    
    and it'll figure out the `^^^^^` bit by re-creating it based on the span
    data, and checking that the string is right. This means if the span data
    is wrong, or you put the `^^^^`s in the wrong place, you get an error
    like:
    
    <img width="623" height="281" alt="Image"
    src="https://github.com/user-attachments/assets/e3c82614-7e86-4736-848f-9a399ea08eba"
    />

--- crates/css_parse/src/test_helpers.rs
+++ crates/css_parse/src/test_helpers.rs
@@ -10,10 +10,29 @@
 /// ```
 /// use css_parse::*;
 /// assert_parse!(T![Ident], "foo");
+/// // Equivalent to:
+/// assert_parse!(T![Ident], "foo", "foo");
+/// ```
+///
+/// For more complex types (for example enum variants), you might want to assert that the given AST
+/// node matches an expected pattern (for example, one enum variant was chosen over another). In
+/// these cases, passing the match pattern as the third (or fourth) argument will assert that the
+/// parsed output struct matches the given pattern:
+///
+/// ```
+/// use css_parse::*;
+/// use csskit_derives::*;
+/// #[derive(Parse, ToCursors, Debug)]
+/// enum IdentOrNumber {
+///     Ident(T![Ident]),
+///     Number(T![Number]),
+/// }
+/// assert_parse!(IdentOrNumber, "foo", IdentOrNumber::Ident(_));
+/// assert_parse!(IdentOrNumber, "12", IdentOrNumber::Number(_));
 /// ```
 #[macro_export]
 macro_rules! assert_parse {
-	($ty: ty, $str: literal, $str2: literal, $($ast: pat)*) => {
+	($ty: ty, $str: literal, $str2: literal, $($ast: pat)+) => {
 		let source_text = $str;
 		let expected = $str2;
 		let bump = ::bumpalo::Bump::default();
@@ -31,16 +50,23 @@ macro_rules! assert_parse {
 		if expected != actual {
 			panic!("\n\nParse failed: did not match expected format:\n\n   parser input: {:?}\n  parser output: {:?}\n       expected: {:?}\n", source_text, actual, expected);
 		}
-		$(assert!(matches!(result.output.unwrap(), $ast));)*
+		if !matches!(result.output, Some($($ast)|+)) {
+			panic!(
+        "\n\nParse succeeded but struct did not match given match pattern:\n\n           input: {:?}\n  match pattern: {}\n  parsed struct: {:#?}\n",
+        source_text,
+        stringify!($($ast)|+),
+        result.output.unwrap(),
+      );
+    }
 	};
 	($ty: ty, $str: literal) => {
-		assert_parse!($ty, $str, $str,);
+		assert_parse!($ty, $str, $str, _);
 	};
 	($ty: ty, $str: literal, $str2: literal) => {
-		assert_parse!($ty, $str, $str2,);
+		assert_parse!($ty, $str, $str2, _);
 	};
-	($ty: ty, $str: literal, $($ast: pat)*) => {
-		assert_parse!($ty, $str, $str, $($ast)*);
+	($ty: ty, $str: literal, $($ast: pat)+) => {
+		assert_parse!($ty, $str, $str, $($ast)+);
 	};
 }
 #[cfg(test)]
@@ -77,3 +103,54 @@ macro_rules! assert_parse_error {
 }
 #[cfg(test)]
 pub(crate) use assert_parse_error;
+
+/// (Requires feature "testing") Given a Node, and a multiline string, this will expand to code that sets up a parser,
+/// and parses the first line of the given string with the parser. It will then create a second string based on the span
+/// data and append it to the first line of the string, showing what was parsed and where the span rests.
+///
+/// This uses `parse`, as it will be often used in situations where there may be trailing unparsed tokens.
+///
+/// ```
+/// use css_parse::*;
+/// assert_parse_span!(T![Ident], r#"
+///	 an_ident another_ident
+///	 ^^^^^^^^
+///	"#);
+/// ```
+#[macro_export]
+macro_rules! assert_parse_span {
+	($ty: ty, $str: literal) => {
+		let expected = $str;
+		let (indent, source_text) = expected
+			.lines()
+			.find(|line| !line.trim().is_empty())
+			.map(|line| {
+				let content = line.trim_start();
+				let ws_len = line.len() - content.len();
+				(&line[..ws_len], content)
+			})
+			.unwrap_or(("", ""));
+		let bump = ::bumpalo::Bump::default();
+		let mut parser = $crate::Parser::new(&bump, source_text);
+		let result = parser.parse::<$ty>();
+		match result {
+			Ok(result) => {
+				use ::css_lexer::ToSpan;
+				let span = result.to_span();
+				let actual = format!("\n{}{}\n{}{}\n", indent, source_text, indent, "^".repeat(span.len() as usize));
+				if expected.trim() != actual.trim() {
+					panic!(
+						"\n\nParse on {}:{} succeeded but span differs:\n\n  expected: {}\n  actual: {}\n",
+						file!(),
+						line!(),
+						actual,
+						expected
+					);
+				}
+			}
+			Err(err) => {
+				panic!("\n\nParse on {}:{} failed. ({:?}) saw error {:?}", file!(), line!(), source_text, err);
+			}
+		}
+	};
+}
