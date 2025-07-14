7b1df8e359b54fb3bc32a13dc89700e6551155e2
Keith Cirkel <keithamus@users.noreply.github.com>
2025-07-14 11:14:06 +0100

    css_ast: zoom is not a set of optionals

--- crates/css_ast/src/values/viewport/mod.rs
+++ crates/css_ast/src/values/viewport/mod.rs
@@ -7,11 +7,11 @@ use impls::*;
  */
 
 // https://drafts.csswg.org/css-viewport-1/#zoom
-#[value(" <number [0,∞]> || <percentage [0,∞]> ")]
+#[value(" <number [0,∞]> | <percentage [0,∞]> ")]
 #[initial("1")]
 #[applies_to("all <length> property values of all elements")]
 #[inherited("no")]
 #[percentages("converted to <number>")]
 #[canonical_order("per grammar")]
 #[animation_type("not animatable")]
-pub struct ZoomStyleValue;
+pub enum ZoomStyleValue {}
