1f011f309df3fc0a4d7dc58775ed7aacab3aba2f
Keith Cirkel <keithamus@users.noreply.github.com>
2025-07-18 20:40:18 +0100

    css_ast: support url()/src() functions in images
    `<image>` has a syntax of `<url>`. `<url>` is a type, distinct to `<url-token>` (the token). The `<url>` type is one of
    `url()` or `src()`. This type also allows for either the `<url-token>` production or cases which do not satisfy that lex
    token, and instead lex as a Function.
    
    This change introduces the new `<url>` type, and refactors `<image>` to use this type.

--- crates/css_ast/src/types/image.rs
+++ crates/css_ast/src/types/image.rs
@@ -1,42 +1,17 @@
-use css_lexer::{Cursor, ToSpan};
-use css_parse::{Parse, Parser, Peek, Result as ParserResult, T, diagnostics};
-use csskit_derives::{ToCursors, ToSpan};
+use csskit_derives::{Parse, Peek, ToCursors, ToSpan};
 
-use super::Gradient;
+use super::{Gradient, Url};
 
-// https://drafts.csswg.org/css-images-3/#typedef-image
-#[derive(ToSpan, ToCursors, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+/// <https://drafts.csswg.org/css-images-3/#typedef-image>
+///
+/// ```text
+/// <image> = <url> | <gradient>
+/// ```
+#[derive(Parse, Peek, ToSpan, ToCursors, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize), serde())]
 pub enum Image<'a> {
-	Url(T![Url]),
-	UrlFunction(T![Function], T![String], T![')']),
+	Url(Url),
 	Gradient(Gradient<'a>),
-}
-
-impl<'a> Peek<'a> for Image<'a> {
-	fn peek(p: &Parser<'a>, c: Cursor) -> bool {
-		<T![Url]>::peek(p, c)
-			|| <Gradient>::peek(p, c)
-			|| (<T![Function]>::peek(p, c) && p.eq_ignore_ascii_case(c, "url"))
-	}
-}
-
-impl<'a> Parse<'a> for Image<'a> {
-	fn parse(p: &mut Parser<'a>) -> ParserResult<Self> {
-		if p.peek::<T![Url]>() {
-			Ok(Self::Url(p.parse::<T![Url]>()?))
-		} else if p.peek::<Gradient>() {
-			return Ok(Self::Gradient(p.parse::<Gradient>()?));
-		} else {
-			let func = p.parse::<T![Function]>()?;
-			if !p.eq_ignore_ascii_case(func.into(), "url") {
-				Err(diagnostics::UnexpectedFunction(p.parse_str(func.into()).into(), func.to_span()))?
-			}
-			let string = p.parse::<T![String]>()?;
-			let close = p.parse::<T![')']>()?;
-			return Ok(Self::UrlFunction(func, string, close));
-		}
-	}
 }
 
 #[cfg(test)]
