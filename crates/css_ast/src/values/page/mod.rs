d4c928fd5c46b860aba67e7f3ae996b3f63db6d1
Keith Cirkel <keithamus@users.noreply.github.com>
2025-08-02 15:48:54 +0000

    Regenerate css_ast/src/values from csswg drafts

--- crates/css_ast/src/values/page/mod.rs
+++ crates/css_ast/src/values/page/mod.rs
@@ -22,7 +22,7 @@ use impls::*;
 #[percentages("n/a")]
 #[canonical_order("per grammar")]
 #[animation_type("discrete")]
-#[popularity(0.335335)]
+#[popularity(0.32973)]
 #[caniuse(Unknown)]
 #[baseline(Unknown)]
 #[versions(Unknown)]
