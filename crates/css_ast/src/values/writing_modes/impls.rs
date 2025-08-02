4d08e8f02eafac0a587da25bd5f5b4c7d8cb63b9
Keith Cirkel <keithamus@users.noreply.github.com>
2025-08-02 22:12:47 +0100

    css_ast: Implement text-combine-upright style value. (#247)
    Thanks to https://github.com/csskit/csskit/pull/243 &
    https://github.com/csskit/csskit/pull/246 we can now uncomment this
    rule!

--- crates/css_ast/src/values/writing_modes/impls.rs
+++ crates/css_ast/src/values/writing_modes/impls.rs
@@ -7,9 +7,13 @@ mod tests {
 	use css_parse::{assert_parse, assert_parse_error};
 
 	#[test]
-	fn test_parse_error() {
-		assert_parse_error!(GlyphOrientationVerticalStyleValue, "128");
-		assert_parse_error!(GlyphOrientationVerticalStyleValue, "50deg");
+	fn size_test() {
+		assert_eq!(std::mem::size_of::<DirectionStyleValue>(), 16);
+		assert_eq!(std::mem::size_of::<UnicodeBidiStyleValue>(), 16);
+		assert_eq!(std::mem::size_of::<WritingModeStyleValue>(), 16);
+		assert_eq!(std::mem::size_of::<TextOrientationStyleValue>(), 16);
+		assert_eq!(std::mem::size_of::<GlyphOrientationVerticalStyleValue>(), 16);
+		assert_eq!(std::mem::size_of::<TextCombineUprightStyleValue>(), 28);
 	}
 
 	#[test]
@@ -18,5 +22,20 @@ mod tests {
 		assert_parse!(GlyphOrientationVerticalStyleValue, "0");
 		assert_parse!(GlyphOrientationVerticalStyleValue, "90");
 		assert_parse!(GlyphOrientationVerticalStyleValue, "90deg");
+		assert_parse!(TextCombineUprightStyleValue, "none");
+		assert_parse!(TextCombineUprightStyleValue, "all");
+		assert_parse!(TextCombineUprightStyleValue, "digits");
+		assert_parse!(TextCombineUprightStyleValue, "digits 2");
+		assert_parse!(TextCombineUprightStyleValue, "digits 4");
+	}
+
+	#[test]
+	fn test_parse_error() {
+		assert_parse_error!(GlyphOrientationVerticalStyleValue, "128");
+		assert_parse_error!(GlyphOrientationVerticalStyleValue, "50deg");
+		assert_parse_error!(GlyphOrientationVerticalStyleValue, "50deg");
+		assert_parse_error!(TextCombineUprightStyleValue, "digits 1");
+		assert_parse_error!(TextCombineUprightStyleValue, "digits 2 2");
+		assert_parse_error!(TextCombineUprightStyleValue, "digits 5");
 	}
 }
