5b46e4fab3368683db989f3519e28b1a4a07d772
Keith Cirkel <keithamus@users.noreply.github.com>
2025-08-25 11:10:07 +0100

    csskit: Get basic fmt, minify commands working.
    The last few PRs have got us the basics of minification and formatting, so we can begin iterating on those commands
    within the main binary.
    
    This change refactors csskit main.rs so that the `fmt` and `min` commands are available and do something.
    
    It also introduces a new mise task for iterating over the `tasks/coverage/basic` css files and comparing the  minified output of `csskit min *.css` against `*.min.css`, and likewise `csskit fmt *.css` against `*.fmt.css`.

--- crates/csskit/src/main.rs
+++ crates/csskit/src/main.rs
@@ -1,10 +1,11 @@
 use bumpalo::Bump;
 use clap::{Parser, Subcommand, crate_version};
 use css_ast::StyleSheet;
-use css_parse::{CursorWriteSink, ToCursors};
+use css_lexer::QuoteStyle;
+use css_parse::{CursorCompactWriteSink, CursorPrettyWriteSink, ToCursors, parse};
 use csskit_lsp::{LSPService, Server};
 use miette::{GraphicalReportHandler, GraphicalTheme, NamedSource};
-use std::io;
+use std::{io, process::exit};
 use tracing::{level_filters::LevelFilter, trace};
 use tracing_subscriber::{Layer, fmt, layer::SubscriberExt, registry, util::SubscriberInitExt};
 
@@ -37,6 +38,34 @@ enum Commands {
 		#[arg(required = true, value_parser)]
 		input: Vec<String>,
 
+		/// Where to save files.
+		#[arg(short, long, group = "output_file", value_parser)]
+		output: Option<String>,
+
+		/// Don't write any files, instead report each change that would have been made.
+		/// This will exit with a non-zero status code if any changes need to be made. Useful for CI.
+		#[arg(short, long, value_parser)]
+		check: bool,
+
+		/// Expand tab characters into a number of spaces.
+		#[arg(short('t'), long, value_parser)]
+		expand_tab: Option<u8>,
+
+		/// Rewrite quotes to single quotes
+		#[arg(long, value_parser)]
+		single_quotes: bool,
+	},
+
+	/// Minify CSS files to compress them optimized delivery.
+	Min {
+		/// A list of CSS files to build. Each input will result in one output file.
+		#[arg(required = true, value_parser)]
+		input: Vec<String>,
+
+		/// Where to save files.
+		#[arg(short, long, group = "output_file", value_parser)]
+		output: Option<String>,
+
 		/// Don't write any files, instead report each change that would have been made.
 		/// This will exit with a non-zero status code if any changes need to be made. Useful for CI.
 		#[arg(short, long, value_parser)]
@@ -58,10 +87,6 @@ enum Commands {
 		#[arg(required = true, value_parser)]
 		input: Vec<String>,
 
-		/// Shorthand to apply all safe transforms, such as removing whitespace or redundant code.
-		#[arg(short, long, value_parser)]
-		minify: bool,
-
 		/// Where to save files.
 		#[arg(short, long, group = "output_file", value_parser)]
 		output: Option<String>,
@@ -79,63 +104,139 @@ fn main() {
 		Commands::Check { input, fix } => {
 			todo!("Check ({:?}, {:?})", input, fix);
 		}
-		Commands::Fmt { input, check } => {
-			todo!("Fmt ({:?}, {:?})", input, check);
-		}
-		Commands::DbgParse { input } => {
-			let source_text = std::fs::read_to_string(input).unwrap();
-			println!("{source_text}");
-			let bump = Bump::default();
-			let result = css_parse::Parser::new(&bump, source_text.as_str()).parse_entirely::<StyleSheet>();
-			if let Some(stylesheet) = &result.output {
-				println!("{stylesheet:#?}");
-			} else {
-				let handler = GraphicalReportHandler::new_themed(GraphicalTheme::unicode_nocolor());
-				for err in result.errors {
-					let mut report = String::new();
-					let named = NamedSource::new(input, source_text.clone());
-					let err = err.with_source_code(named);
-					handler.render_report(&mut report, err.as_ref()).unwrap();
-					println!("{report}");
-				}
-			}
-		}
-		Commands::Build { input, minify, output } => {
-			if input.len() > 1 {
-				todo!("Can't handle multiple files yet")
-			}
 
-			let file_name = input.first().unwrap();
-			let source_text = std::fs::read_to_string(file_name).unwrap();
+		Commands::Fmt { input, output, check, expand_tab, single_quotes } => {
 			let bump = Bump::default();
 			let start = std::time::Instant::now();
-			let result = css_parse::Parser::new(&bump, source_text.as_str()).parse_entirely::<StyleSheet>();
-			{
+			let quotes = if *single_quotes { QuoteStyle::Single } else { QuoteStyle::Double };
+			if *check && output.is_some() {
+				eprintln!("Ignoring output option, because check was passed");
+			}
+			let mut exit_code = 0;
+			for file_name in input.iter() {
+				let source_string = std::fs::read_to_string(file_name).unwrap();
+				let source_text = source_string.as_str();
+				let result = parse!(in bump &source_text as StyleSheet);
 				if result.output.is_some() {
 					let mut str = String::new();
-					let mut stream = CursorWriteSink::new(source_text.as_str(), &mut str);
+					let mut stream = CursorPrettyWriteSink::new(source_text, &mut str, *expand_tab, quotes);
 					result.to_cursors(&mut stream);
-					if let Some(file) = output {
+					if *check {
+						if str != source_text {
+							println!("{str}");
+							exit_code += 1;
+						}
+					} else if let Some(file) = output {
 						std::fs::write(file, str.as_bytes()).unwrap();
 					} else {
 						println!("{str}");
-						eprintln!("Slurped up CSS in {:?}! Neat!", start.elapsed());
-						if *minify {
-							eprintln!("Warning: minification not yet supported");
-						}
 					}
 				} else {
 					let handler = GraphicalReportHandler::new_themed(GraphicalTheme::unicode_nocolor());
 					for err in result.errors {
 						let mut report = String::new();
-						let named = NamedSource::new(file_name, source_text.clone());
+						let named = NamedSource::new(file_name, source_string.clone());
 						let err = err.with_source_code(named);
 						handler.render_report(&mut report, err.as_ref()).unwrap();
 						println!("{report}");
 					}
 				}
 			}
+			eprintln!("Slurped up CSS in {:?}! Neat!", start.elapsed());
+			exit(exit_code)
 		}
+
+		Commands::Min { input, output, check } => {
+			let bump = Bump::default();
+			let start = std::time::Instant::now();
+			if *check && output.is_some() {
+				eprintln!("Ignoring output option, because check was passed");
+			}
+			let mut exit_code = 0;
+			for file_name in input.iter() {
+				let source_string = std::fs::read_to_string(file_name).unwrap();
+				let source_text = source_string.as_str();
+				let result = parse!(in bump &source_text as StyleSheet);
+				if result.output.is_some() {
+					let mut str = String::new();
+					let mut stream = CursorCompactWriteSink::new(source_text, &mut str);
+					result.to_cursors(&mut stream);
+					if *check {
+						if str != source_text {
+							println!("{str}");
+							exit_code += 1;
+						}
+					} else if let Some(file) = output {
+						std::fs::write(file, str.as_bytes()).unwrap();
+					} else {
+						println!("{str}");
+					}
+				} else {
+					let handler = GraphicalReportHandler::new_themed(GraphicalTheme::unicode_nocolor());
+					for err in result.errors {
+						let mut report = String::new();
+						let named = NamedSource::new(file_name, source_string.clone());
+						let err = err.with_source_code(named);
+						handler.render_report(&mut report, err.as_ref()).unwrap();
+						println!("{report}");
+					}
+				}
+			}
+			eprintln!("Slurped up CSS in {:?}! Neat!", start.elapsed());
+			exit(exit_code)
+		}
+
+		Commands::DbgParse { input } => {
+			let source_string = std::fs::read_to_string(input).unwrap();
+			let source_text = source_string.as_str();
+			println!("{source_text}");
+			let bump = Bump::default();
+			let result = parse!(in bump &source_text as StyleSheet);
+			if let Some(stylesheet) = &result.output {
+				println!("{stylesheet:#?}");
+			} else {
+				let handler = GraphicalReportHandler::new_themed(GraphicalTheme::unicode_nocolor());
+				for err in result.errors {
+					let mut report = String::new();
+					let named = NamedSource::new(input, source_string.clone());
+					let err = err.with_source_code(named);
+					handler.render_report(&mut report, err.as_ref()).unwrap();
+					println!("{report}");
+				}
+			}
+		}
+
+		Commands::Build { input, output } => {
+			let bump = Bump::default();
+			let mut str = String::new();
+			let start = std::time::Instant::now();
+			for file_name in input.iter() {
+				let source_string = std::fs::read_to_string(file_name).unwrap();
+				let source_text = source_string.as_str();
+				let mut stream = CursorCompactWriteSink::new(source_text, &mut str);
+				let result = parse!(in bump &source_text as StyleSheet);
+				if result.output.is_some() {
+					result.to_cursors(&mut stream);
+				} else {
+					let handler = GraphicalReportHandler::new_themed(GraphicalTheme::unicode_nocolor());
+					for err in result.errors {
+						let mut report = String::new();
+						let named = NamedSource::new(file_name, source_string.clone());
+						let err = err.with_source_code(named);
+						handler.render_report(&mut report, err.as_ref()).unwrap();
+						println!("{report}");
+					}
+					exit(1);
+				}
+			}
+			if let Some(file) = output {
+				std::fs::write(file, str.as_bytes()).unwrap();
+			} else {
+				println!("{str}");
+			}
+			eprintln!("Slurped up CSS in {:?}! Neat!", start.elapsed());
+		}
+
 		Commands::Lsp {} => {
 			let server = Server::new(LSPService::new(crate_version!()));
 			let stderr_log = fmt::layer().with_writer(io::stderr).with_filter(if debug {
