b1daf0cbc5a976daa82f4f0de4a0c3c1aa05c937
Keith Cirkel <keithamus@users.noreply.github.com>
2025-08-30 11:13:13 +0100

    css_ast: Use #[syntax] for FontWeightAbsolute

--- crates/css_ast/src/types/font_weight_absolute.rs
+++ crates/css_ast/src/types/font_weight_absolute.rs
@@ -1,54 +1,21 @@
-use css_parse::{Cursor, Parse, Parser, Peek, Result as ParserResult, T, diagnostics};
-use csskit_derives::{IntoCursor, ToCursors, Visitable};
+use csskit_derives::{IntoCursor, Peek, ToCursors, Visitable};
+use csskit_proc_macro::syntax;
 
-use crate::CSSInt;
-
-// https://drafts.csswg.org/css-fonts-4/#font-weight-absolute-values
-// <font-weight-absolute> = [normal | bold | <number [1,1000]>]
-#[derive(IntoCursor, ToCursors, Visitable, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+/// <https://drafts.csswg.org/css-fonts-4/#font-weight-absolute-values>
+///
+/// ```text,ignore
+/// <font-weight-absolute> = [normal | bold | <number [1,1000]>]
+/// ```
+#[syntax(" normal | bold | <number [1,1000]> ")]
+#[derive(Peek, IntoCursor, ToCursors, Visitable, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize), serde(tag = "type", content = "value"))]
 #[visit(self)]
-pub enum FontWeightAbsolute {
-	Normal(T![Ident]),
-	Bold(T![Ident]),
-	Number(CSSInt),
-}
-
-impl<'a> Peek<'a> for FontWeightAbsolute {
-	fn peek(p: &Parser<'a>, c: Cursor) -> bool {
-		<CSSInt>::peek(p, c)
-			|| (<T![Ident]>::peek(p, c) && (p.eq_ignore_ascii_case(c, "normal") || p.eq_ignore_ascii_case(c, "bold")))
-	}
-}
-
-impl<'a> Parse<'a> for FontWeightAbsolute {
-	fn parse(p: &mut Parser<'a>) -> ParserResult<Self> {
-		if p.peek::<T![Ident]>() {
-			if p.eq_ignore_ascii_case(p.peek_n(1), "normal") {
-				return Ok(Self::Normal(p.parse::<T![Ident]>()?));
-			}
-			if p.eq_ignore_ascii_case(p.peek_n(1), "bold") {
-				return Ok(Self::Bold(p.parse::<T![Ident]>()?));
-			}
-		}
-		let int = p.parse::<CSSInt>()?;
-		let f: f32 = int.into();
-		if f < 1.0 {
-			let c: Cursor = int.into();
-			Err(diagnostics::NumberTooSmall(f, c.into()))?
-		}
-		if f > 1000.0 {
-			let c: Cursor = int.into();
-			Err(diagnostics::NumberTooLarge(f, c.into()))?
-		}
-		Ok(Self::Number(int))
-	}
-}
+pub enum FontWeightAbsolute {}
 
 #[cfg(test)]
 mod tests {
 	use super::*;
-	use css_parse::assert_parse;
+	use css_parse::{assert_parse, assert_parse_error};
 
 	#[test]
 	fn size_test() {
@@ -62,5 +29,11 @@ mod tests {
 		assert_parse!(FontWeightAbsolute, "100");
 		assert_parse!(FontWeightAbsolute, "500");
 		assert_parse!(FontWeightAbsolute, "900");
+		assert_parse!(FontWeightAbsolute, "900.5");
+	}
+
+	#[test]
+	fn test_errors() {
+		assert_parse_error!(FontWeightAbsolute, "1000.1");
 	}
 }
