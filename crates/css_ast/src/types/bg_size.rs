bff85e40589ef03e521ddef1cc6cbeb120120c87
Keith Cirkel <keithamus@users.noreply.github.com>
2025-08-30 10:59:48 +0100

    css_ast: Use #[syntax] for BgSize

--- crates/css_ast/src/types/bg_size.rs
+++ crates/css_ast/src/types/bg_size.rs
@@ -1,23 +1,17 @@
 use css_parse::keyword_set;
-use csskit_derives::{Parse, Peek, ToCursors, ToSpan, Visitable};
-
-use crate::{AutoOr, LengthPercentage};
+use csskit_derives::{Peek, ToCursors, ToSpan, Visitable};
+use csskit_proc_macro::syntax;
 
 /// <https://drafts.csswg.org/css-backgrounds-3/#typedef-bg-size>
 ///
 /// ```text,ignore
 /// <bg-size> = [ <length-percentage [0,∞]> | auto ]{1,2} | cover | contain
 /// ```
-#[derive(Parse, Peek, ToCursors, ToSpan, Visitable, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[syntax(" [ <length-percentage [0,∞]> | auto ]{1,2} | cover | contain ")]
+#[derive(Peek, ToCursors, ToSpan, Visitable, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize), serde(rename_all = "kebab-case"))]
 #[visit]
-pub enum BgSize {
-	LengthPercentage(AutoOr<LengthPercentage>, Option<AutoOr<LengthPercentage>>),
-	#[visit(skip)]
-	Cover(CoverKeyword),
-	#[visit(skip)]
-	Contain(ContainKeyword),
-}
+pub enum BgSize {}
 
 keyword_set!(pub struct CoverKeyword "cover");
 keyword_set!(pub struct ContainKeyword "contain");
@@ -34,11 +28,11 @@ mod tests {
 
 	#[test]
 	fn test_writes() {
-		assert_parse!(BgSize, "cover");
-		assert_parse!(BgSize, "contain");
-		assert_parse!(BgSize, "12%");
-		assert_parse!(BgSize, "auto auto");
-		assert_parse!(BgSize, "12% 10px");
+		assert_parse!(BgSize, "cover", BgSize::Cover(_));
+		assert_parse!(BgSize, "contain", BgSize::Contain(_));
+		assert_parse!(BgSize, "12%", BgSize::AutoOrLengthPercentage(_, _));
+		assert_parse!(BgSize, "auto auto", BgSize::AutoOrLengthPercentage(_, _));
+		assert_parse!(BgSize, "12% 10px", BgSize::AutoOrLengthPercentage(_, _));
 	}
 
 	#[test]
