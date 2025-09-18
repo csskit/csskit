1b30da049b0ab0bd1311258bebcc888346694540
Keith Cirkel <keithamus@users.noreply.github.com>
2025-09-18 22:03:33 +0100

    css_ast/css_parse/css_lexer: Add allocation tests
    These tests confirm that the only allocations that should be happening are within the memory arena and not in the
    general purpose allocator. They also are beneficial for helping manage exactly how much memory certain test files will
    consume.

---
+++ crates/css_lexer/tests/allocations.rs
@@ -0,0 +1,62 @@
+use css_lexer::{Kind, Lexer};
+#[cfg(feature = "_dhat-heap-testing")]
+use dhat::{Alloc, HeapStats, Profiler, assert_eq};
+#[cfg(not(feature = "_dhat-heap-testing"))]
+use std::assert_eq;
+use std::fs::read_to_string;
+
+#[cfg(feature = "_dhat-heap-testing")]
+#[global_allocator]
+static ALLOC: Alloc = Alloc;
+
+#[test]
+fn ensure_no_allocations_during_lex() {
+	let str = "body{color:blue}";
+	let mut lexer = Lexer::new(&str);
+	let mut token_count = 0;
+
+	let str2 = "bo\\d y{\\75\\52\\6c(a);width:1\\70\\78}";
+	let mut token_count2 = 0;
+	let mut lexer2 = Lexer::new(&str2);
+
+	let big = read_to_string("../../coverage/popular/tailwind.2.2.19.min.css").unwrap();
+	let mut lexer_big = Lexer::new(&big);
+
+	#[cfg(feature = "_dhat-heap-testing")]
+	let _profiler = Profiler::builder()
+		.testing()
+		.file_name(format!("../../target/css_lexer_allocations_test-{}.json", std::process::id()))
+		.build();
+
+	loop {
+		let cursor = lexer.advance();
+		token_count += 1;
+		if cursor.kind() == Kind::Eof {
+			break;
+		}
+	}
+	assert_eq!(token_count, 7);
+
+	loop {
+		let cursor = lexer2.advance();
+		token_count2 += 1;
+		if cursor.kind() == Kind::Eof {
+			break;
+		}
+	}
+	assert_eq!(token_count2, 9);
+
+	loop {
+		let cursor = lexer_big.advance();
+		if cursor.kind() == Kind::Eof {
+			break;
+		}
+	}
+
+	#[cfg(feature = "_dhat-heap-testing")]
+	{
+		let stats = HeapStats::get();
+		assert_eq!(stats.total_blocks, 0);
+		assert_eq!(stats.total_bytes, 0);
+	}
+}
---
+++ crates/css_parse/tests/allocations.rs
@@ -0,0 +1,63 @@
+use bumpalo::Bump;
+use css_parse::ComponentValues;
+use css_parse::Parser;
+#[cfg(feature = "_dhat-heap-testing")]
+use dhat::{Alloc, HeapStats, Profiler, assert_eq};
+use std::fs::read_to_string;
+
+#[cfg(feature = "_dhat-heap-testing")]
+#[global_allocator]
+static ALLOC: Alloc = Alloc;
+
+#[test]
+fn allocation_test() {
+	let simple_bump_size = 960;
+	let simple_bump = Bump::with_capacity(simple_bump_size);
+	let simple_str = "body{color:blue}";
+	let mut simple_parser = Parser::new(&simple_bump, &simple_str);
+
+	let escaped_bump_size = 960;
+	let escaped_bump = Bump::with_capacity(escaped_bump_size);
+	let escaped_str = "bo\\d y{background-image:\\75\\52\\6c(a);width:1\\70\\78}";
+	let mut escape_parser = Parser::new(&escaped_bump, &escaped_str);
+
+	let big_bump_size = 100_003_776;
+	let big_bump = Bump::with_capacity(big_bump_size);
+	let big_str = read_to_string("../../coverage/popular/tailwind.2.2.19.min.css").unwrap();
+	let mut big_parser = Parser::new(&big_bump, &big_str);
+
+	#[cfg(feature = "_dhat-heap-testing")]
+	let _profiler = Profiler::builder()
+		.testing()
+		.file_name(format!("../../target/css_parse_allocations_test-{}.json", std::process::id()))
+		.build();
+
+	simple_parser.parse_entirely::<ComponentValues>();
+	#[cfg(feature = "_dhat-heap-testing")]
+	{
+		let stats = HeapStats::get();
+		assert_eq!(stats.total_blocks, 0);
+		assert_eq!(stats.total_bytes, 0);
+	}
+
+	escape_parser.parse_entirely::<ComponentValues>();
+	#[cfg(feature = "_dhat-heap-testing")]
+	{
+		let stats = HeapStats::get();
+		assert_eq!(stats.total_blocks, 0);
+		assert_eq!(stats.total_bytes, 0);
+	}
+
+	big_parser.parse_entirely::<ComponentValues>();
+	#[cfg(feature = "_dhat-heap-testing")]
+	{
+		let stats = HeapStats::get();
+		assert_eq!(stats.total_blocks, 0);
+		assert_eq!(stats.total_bytes, 0);
+	}
+
+	// XXX: If these fail because the numbers go down, great! If they go up, investigate why.
+	assert_eq!(simple_bump.allocated_bytes(), simple_bump_size);
+	assert_eq!(escaped_bump.allocated_bytes(), escaped_bump_size);
+	assert_eq!(big_bump.allocated_bytes(), big_bump_size);
+}
