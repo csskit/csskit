98d023c80e8d2ae0bf30662feb9a8cea6842cf29
Keith Cirkel <keithamus@users.noreply.github.com>
2025-10-25 23:09:59 +0100

    benchmarks: fix benchmark code
    This broke in #563 due to the signature change to Parser::new

--- crates/css_ast/benches/parse_popular.rs
+++ crates/css_ast/benches/parse_popular.rs
@@ -1,6 +1,7 @@
 use bumpalo::Bump;
 use criterion::{BenchmarkId, Criterion, Throughput, criterion_group, criterion_main};
 use css_ast::{CssAtomSet, StyleSheet};
+use css_lexer::Lexer;
 use css_parse::Parser;
 use glob::glob;
 #[cfg(target_family = "unix")]
@@ -32,7 +33,8 @@ fn popular(c: &mut Criterion) {
 		group.bench_with_input(BenchmarkId::from_parameter(&file.name), &file.source_text, |b, source_text| {
 			b.iter_with_large_drop(|| {
 				let allocator = Bump::default();
-				let _ = Parser::new(&allocator, &CssAtomSet::ATOMS, source_text).parse_entirely::<StyleSheet>();
+				let lexer = Lexer::new(&CssAtomSet::ATOMS, &source_text);
+				let _ = Parser::new(&allocator, source_text, lexer).parse_entirely::<StyleSheet>();
 
 				allocator
 			});
