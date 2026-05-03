e5921c01d325171d9b6dbe9d29cdc4b2b2e4c078
Keith Cirkel <keithamus@users.noreply.github.com>
2026-05-03 20:25:04 +0100

    css_ast: Add tests for FlexWrapStyleValue

--- crates/css_ast/src/values/flexbox/impls.rs
+++ crates/css_ast/src/values/flexbox/impls.rs
@@ -2,7 +2,7 @@
 mod tests {
 	use super::super::*;
 	use crate::CssAtomSet;
-	use css_parse::assert_parse;
+	use css_parse::{assert_parse, assert_parse_error};
 
 	#[test]
 	fn size_test() {
@@ -13,6 +13,28 @@ mod tests {
 		assert_eq!(std::mem::size_of::<FlexGrowStyleValue>(), 12);
 		assert_eq!(std::mem::size_of::<FlexShrinkStyleValue>(), 12);
 		assert_eq!(std::mem::size_of::<FlexBasisStyleValue>(), 40);
+	}
+
+	#[test]
+	fn test_flex_wrap_writes() {
+		assert_parse!(CssAtomSet::ATOMS, FlexWrapStyleValue, "nowrap");
+		assert_parse!(CssAtomSet::ATOMS, FlexWrapStyleValue, "wrap");
+		assert_parse!(CssAtomSet::ATOMS, FlexWrapStyleValue, "wrap-reverse");
+		assert_parse!(CssAtomSet::ATOMS, FlexWrapStyleValue, "balance");
+	}
+
+	#[test]
+	fn test_flex_wrap_errors() {
+		assert_parse_error!(CssAtomSet::ATOMS, FlexWrapStyleValue, "none");
+		assert_parse_error!(CssAtomSet::ATOMS, FlexWrapStyleValue, "reverse");
+	}
+
+	#[test]
+	fn test_flex_direction_writes() {
+		assert_parse!(CssAtomSet::ATOMS, FlexDirectionStyleValue, "row");
+		assert_parse!(CssAtomSet::ATOMS, FlexDirectionStyleValue, "row-reverse");
+		assert_parse!(CssAtomSet::ATOMS, FlexDirectionStyleValue, "column");
+		assert_parse!(CssAtomSet::ATOMS, FlexDirectionStyleValue, "column-reverse");
 	}
 
 	#[test]
