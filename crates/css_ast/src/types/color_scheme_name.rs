8810df0944291bbd7a46caca08e5a665512a0259
Keith Cirkel <keithamus@users.noreply.github.com>
2026-06-05 00:11:27 +0100

    css_ast: Implement color-scheme (#1176)

---
+++ crates/css_ast/src/types/color_scheme_name.rs
@@ -0,0 +1,42 @@
+use super::prelude::*;
+
+/// <https://drafts.csswg.org/css-color-adjust-1/#typedef-color-scheme-name>
+///
+/// ```text,ignore
+/// <color-scheme-name> = light | dark | <custom-ident>
+/// ```
+#[derive(Parse, Peek, ToCursors, ToSpan, SemanticEq, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize), serde())]
+#[cfg_attr(feature = "visitable", derive(csskit_derives::Visitable), visit(self))]
+#[derive(csskit_derives::NodeWithMetadata)]
+pub enum ColorSchemeName {
+	#[atom(CssAtomSet::Light)]
+	Light(T![Ident]),
+	#[atom(CssAtomSet::Dark)]
+	Dark(T![Ident]),
+	Other(T![Ident]),
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::CssAtomSet;
+	use css_parse::{assert_parse, assert_parse_error};
+
+	#[test]
+	fn size_test() {
+		assert_eq!(std::mem::size_of::<ColorSchemeName>(), 16);
+	}
+
+	#[test]
+	fn test_writes() {
+		assert_parse!(CssAtomSet::ATOMS, ColorSchemeName, "light");
+		assert_parse!(CssAtomSet::ATOMS, ColorSchemeName, "dark");
+		assert_parse!(CssAtomSet::ATOMS, ColorSchemeName, "foo");
+	}
+
+	#[test]
+	fn test_errors() {
+		assert_parse_error!(CssAtomSet::ATOMS, ColorSchemeName, "123");
+	}
+}
