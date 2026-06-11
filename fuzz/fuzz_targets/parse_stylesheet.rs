f3f66f6d8994a8dec176a879096a50ac35688efe
Keith Cirkel <keithamus@users.noreply.github.com>
2026-06-11 23:26:39 +0100

    fuzz: Try setting up a fuzzing suite

---
+++ fuzz/fuzz_targets/parse_stylesheet.rs
@@ -0,0 +1,24 @@
+#![no_main]
+use bumpalo::Bump;
+use css_ast::{CssAtomSet, StyleSheet};
+use css_lexer::Lexer;
+use css_parse::{CursorOrderedSink, CursorWriteSink, Parser, ToCursors};
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|data: &[u8]| {
+	if data.len() > 4096 {
+		return;
+	}
+	let Ok(source) = std::str::from_utf8(data) else { return };
+
+	let bump = Bump::new();
+	let lexer = Lexer::new(&CssAtomSet::ATOMS, source);
+	let mut parser = Parser::new(&bump, source, lexer);
+	let result = parser.parse_entirely::<StyleSheet>().with_trivia();
+	if let Some(node) = result.output {
+		let mut out = bumpalo::collections::String::new_in(&bump);
+		let mut write_sink = CursorWriteSink::new(source, &mut out);
+		let mut ordered_sink = CursorOrderedSink::new(&bump, &mut write_sink);
+		node.to_cursors(&mut ordered_sink);
+	}
+});
