4bccd2be4e31948b9786a7285e9619ee04d9f619
Keith Cirkel <keithamus@users.noreply.github.com>
2026-07-19 10:03:34 +0100

    css_parse: Drop CursorSource trait (#1296)
    This trait is just dead code. ToCursors doesn't use it.

--- crates/css_parse/src/traits/cursor_source.rs
+++
@@ -1,8 +0,0 @@
-use crate::Cursor;
-
-/// This trait provides the generic `impl` that [ToCursors][crate::ToCursors] can use. This provides just enough API
-/// surface for nodes to put the cursors they represent into some buffer which can later be read, the details of which
-/// are elided.
-pub trait CursorSource {
-	fn iter_cursors(&self) -> impl Iterator<Item = &Cursor>;
-}
