a0edaead444aefd8a7a62e9c62af6425317cdb25
Keith Cirkel <keithamus@users.noreply.github.com>
2026-08-05 15:43:45 +0200

    css_ast: Implement fade() function (#1352)

--- crates/css_ast/src/functions/fade_function.rs
+++ crates/css_ast/src/functions/fade_function.rs
@@ -1,11 +1,41 @@
-#![allow(unused)]
 use super::prelude::*;
+use crate::{CalcableValue, LengthPercentage};
 
-use crate::Todo;
-
-/// <https://drafts.csswg.org/css-overflow-4/#typedef-fade>
+/// <https://drafts.csswg.org/css-overflow-4/#funcdef-text-overflow-fade>
 ///
-/// ```text,ignore
+/// ```text
 /// <fade()> = fade( <length-percentage> )
 /// ```
-pub type FadeFunction = Todo;
+#[node]
+#[derive(Parse, Peek, ToCursors, ToSpan, SemanticEq, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize), serde())]
+#[cfg_attr(feature = "visitable", derive(csskit_derives::Visitable), visit(self))]
+#[derive(csskit_derives::NodeWithMetadata)]
+pub struct FadeFunction<'a> {
+	#[atom(CssAtomSet::Fade)]
+	pub name: T![Function],
+	pub params: CalcableValue<'a, LengthPercentage>,
+	#[semantic_eq(skip)]
+	pub close: T![')'],
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::CssAtomSet;
+	use css_parse::{assert_parse, assert_parse_error};
+
+	#[test]
+	fn test_writes() {
+		assert_parse!(CssAtomSet::ATOMS, FadeFunction, "fade(1px)");
+		assert_parse!(CssAtomSet::ATOMS, FadeFunction, "fade(10%)");
+		assert_parse!(CssAtomSet::ATOMS, FadeFunction, "fade(var(--x))");
+		assert_parse!(CssAtomSet::ATOMS, FadeFunction, "fade(calc(100% - 20px))");
+	}
+
+	#[test]
+	fn test_errors() {
+		assert_parse_error!(CssAtomSet::ATOMS, FadeFunction, "fade()");
+		assert_parse_error!(CssAtomSet::ATOMS, FadeFunction, "fade(red)");
+	}
+}
