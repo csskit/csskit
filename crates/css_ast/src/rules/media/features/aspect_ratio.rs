b0db02d50c89bf072de769d4f3807f5cc74540a3
Keith Cirkel <keithamus@users.noreply.github.com>
2026-08-06 10:37:50 +0100

    css_ast: Implement remaining todo media features

--- crates/css_ast/src/rules/media/features/aspect_ratio.rs
+++ crates/css_ast/src/rules/media/features/aspect_ratio.rs
@@ -1,3 +1,47 @@
-use crate::Todo;
+use super::prelude::*;
+use crate::types::Ratio;
 
-pub type AspectRatioMediaFeature = Todo;
+ranged_feature!(
+	#[node]
+	#[derive(ToCursors, ToSpan, SemanticEq, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+	#[cfg_attr(feature = "serde", derive(serde::Serialize), serde())]
+	#[derive(csskit_derives::FeatureMetadata)]
+	#[feature_metadata(CssAtomSet::AspectRatio)]
+	#[cfg_attr(feature = "visitable", derive(csskit_derives::Visitable), visit)]
+	#[derive(csskit_derives::NodeWithMetadata)]
+	pub enum AspectRatioMediaFeature<'a>{CssAtomSet::AspectRatio | CssAtomSet::MinAspectRatio | CssAtomSet::MaxAspectRatio, Ratio<'a>}
+);
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::CssAtomSet;
+	use css_parse::{assert_parse, assert_parse_error};
+
+	#[test]
+	fn test_writes() {
+		assert_parse!(CssAtomSet::ATOMS, AspectRatioMediaFeature, "(aspect-ratio:1/1)");
+		assert_parse!(CssAtomSet::ATOMS, AspectRatioMediaFeature, "(aspect-ratio:16/9)");
+		assert_parse!(CssAtomSet::ATOMS, AspectRatioMediaFeature, "(aspect-ratio:1)");
+		assert_parse!(CssAtomSet::ATOMS, AspectRatioMediaFeature, "(min-aspect-ratio:1/1)");
+		assert_parse!(CssAtomSet::ATOMS, AspectRatioMediaFeature, "(max-aspect-ratio:16/9)");
+		assert_parse!(CssAtomSet::ATOMS, AspectRatioMediaFeature, "(aspect-ratio<=1/1)");
+		assert_parse!(CssAtomSet::ATOMS, AspectRatioMediaFeature, "(aspect-ratio>=16/9)");
+		assert_parse!(CssAtomSet::ATOMS, AspectRatioMediaFeature, "(aspect-ratio=1/1)");
+		assert_parse!(CssAtomSet::ATOMS, AspectRatioMediaFeature, "(16/9=aspect-ratio)");
+		assert_parse!(CssAtomSet::ATOMS, AspectRatioMediaFeature, "(1/1<=aspect-ratio)");
+		assert_parse!(CssAtomSet::ATOMS, AspectRatioMediaFeature, "(1/1<aspect-ratio<16/9)");
+		assert_parse!(CssAtomSet::ATOMS, AspectRatioMediaFeature, "(1/1>=aspect-ratio<=16/9)");
+	}
+
+	#[test]
+	fn test_errors() {
+		assert_parse_error!(CssAtomSet::ATOMS, AspectRatioMediaFeature, "(aspect-ratio:)");
+		assert_parse_error!(CssAtomSet::ATOMS, AspectRatioMediaFeature, "(aspect-ratio: > 1/1)");
+		assert_parse_error!(CssAtomSet::ATOMS, AspectRatioMediaFeature, "(max-aspect-ratio > 1/1)");
+		assert_parse_error!(CssAtomSet::ATOMS, AspectRatioMediaFeature, "(min-aspect-ratio > 1/1)");
+		assert_parse_error!(CssAtomSet::ATOMS, AspectRatioMediaFeature, "(aspect-ratio: 1px)");
+		assert_parse_error!(CssAtomSet::ATOMS, AspectRatioMediaFeature, "(aspect-ratio: portrait)");
+		assert_parse_error!(CssAtomSet::ATOMS, AspectRatioMediaFeature, "(pointer: 1/1)");
+	}
+}
--- crates/css_ast/src/rules/media/features/device_aspect_ratio.rs
+++ crates/css_ast/src/rules/media/features/device_aspect_ratio.rs
@@ -1,3 +1,44 @@
-use crate::Todo;
+use super::prelude::*;
+use crate::types::Ratio;
 
-pub type DeviceAspectRatioMediaFeature = Todo;
+ranged_feature!(
+	#[node]
+	#[derive(ToCursors, ToSpan, SemanticEq, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+	#[cfg_attr(feature = "serde", derive(serde::Serialize), serde())]
+	#[derive(csskit_derives::FeatureMetadata)]
+	#[feature_metadata(CssAtomSet::DeviceAspectRatio)]
+	#[cfg_attr(feature = "visitable", derive(csskit_derives::Visitable), visit)]
+	#[derive(csskit_derives::NodeWithMetadata)]
+	pub enum DeviceAspectRatioMediaFeature<'a>{CssAtomSet::DeviceAspectRatio | CssAtomSet::MinDeviceAspectRatio | CssAtomSet::MaxDeviceAspectRatio, Ratio<'a>}
+);
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::CssAtomSet;
+	use css_parse::{assert_parse, assert_parse_error};
+
+	#[test]
+	fn test_writes() {
+		assert_parse!(CssAtomSet::ATOMS, DeviceAspectRatioMediaFeature, "(device-aspect-ratio:1/1)");
+		assert_parse!(CssAtomSet::ATOMS, DeviceAspectRatioMediaFeature, "(device-aspect-ratio:16/9)");
+		assert_parse!(CssAtomSet::ATOMS, DeviceAspectRatioMediaFeature, "(min-device-aspect-ratio:1/1)");
+		assert_parse!(CssAtomSet::ATOMS, DeviceAspectRatioMediaFeature, "(max-device-aspect-ratio:16/9)");
+		assert_parse!(CssAtomSet::ATOMS, DeviceAspectRatioMediaFeature, "(device-aspect-ratio<=1/1)");
+		assert_parse!(CssAtomSet::ATOMS, DeviceAspectRatioMediaFeature, "(device-aspect-ratio>=16/9)");
+		assert_parse!(CssAtomSet::ATOMS, DeviceAspectRatioMediaFeature, "(device-aspect-ratio=1/1)");
+		assert_parse!(CssAtomSet::ATOMS, DeviceAspectRatioMediaFeature, "(16/9=device-aspect-ratio)");
+		assert_parse!(CssAtomSet::ATOMS, DeviceAspectRatioMediaFeature, "(1/1<=device-aspect-ratio)");
+		assert_parse!(CssAtomSet::ATOMS, DeviceAspectRatioMediaFeature, "(1/1<device-aspect-ratio<16/9)");
+	}
+
+	#[test]
+	fn test_errors() {
+		assert_parse_error!(CssAtomSet::ATOMS, DeviceAspectRatioMediaFeature, "(device-aspect-ratio:)");
+		assert_parse_error!(CssAtomSet::ATOMS, DeviceAspectRatioMediaFeature, "(device-aspect-ratio: > 1/1)");
+		assert_parse_error!(CssAtomSet::ATOMS, DeviceAspectRatioMediaFeature, "(max-device-aspect-ratio > 1/1)");
+		assert_parse_error!(CssAtomSet::ATOMS, DeviceAspectRatioMediaFeature, "(min-device-aspect-ratio > 1/1)");
+		assert_parse_error!(CssAtomSet::ATOMS, DeviceAspectRatioMediaFeature, "(device-aspect-ratio: 1px)");
+		assert_parse_error!(CssAtomSet::ATOMS, DeviceAspectRatioMediaFeature, "(aspect-ratio: 1/1)");
+	}
+}
