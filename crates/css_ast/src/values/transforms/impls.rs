87376b26468719ed79148c7161f6f23c23543361
Keith Cirkel <keithamus@users.noreply.github.com>
2026-08-09 12:14:01 +0100

    css_ast: Implement RotateStyleValue

--- crates/css_ast/src/values/transforms/impls.rs
+++ crates/css_ast/src/values/transforms/impls.rs
@@ -1,5 +1,5 @@
-use super::TransformOriginStyleValue;
-use crate::{CalcableValue, Length, PositionOne, PositionTwo};
+use super::{RotateStyleValue, TransformOriginStyleValue};
+use crate::{Angle, CalcableValue, CssAtomSet, KeywordValue, Length, Number, NumericValue, PositionOne, PositionTwo};
 use css_parse::{Cursor, Diagnostic, Parse, Parser, Peek, Result as ParseResult, T};
 
 impl<'a> Parse<'a> for TransformOriginStyleValue<'a> {
@@ -15,6 +15,49 @@ impl<'a> Parse<'a> for TransformOriginStyleValue<'a> {
 		let second = p.parse::<PositionOne>()?;
 		let two = PositionTwo::from_two(p, first, second, second_c)?;
 		Ok(Self::PositionTwo(crate::Value::Literal(two), p.parse_if_peek::<CalcableValue<Length>>()?))
+	}
+}
+
+type Vector<'a> = (NumericValue<'a, Number>, NumericValue<'a, Number>, NumericValue<'a, Number>);
+
+impl<'a> Parse<'a> for RotateStyleValue<'a> {
+	fn parse<I>(p: &mut Parser<'a, I>) -> ParseResult<Self>
+	where
+		I: Iterator<Item = Cursor> + Clone,
+	{
+		let c = p.peek_n(1);
+		if <T![Ident]>::peek(p, c) {
+			if p.equals_atom(c, &CssAtomSet::None) {
+				return p.parse::<KeywordValue<'a, T![Ident]>>().map(Self::None);
+			}
+			let variant = match p.to_atom::<CssAtomSet>(c) {
+				CssAtomSet::X => RotateStyleValue::XAngle,
+				CssAtomSet::Y => RotateStyleValue::YAngle,
+				CssAtomSet::Z => RotateStyleValue::ZAngle,
+				_ => Err(Diagnostic::new(c, Diagnostic::unexpected))?,
+			};
+			let keyword = p.parse::<KeywordValue<'a, T![Ident]>>()?;
+			return Ok(variant(keyword, p.parse::<CalcableValue<'a, Angle>>()?));
+		}
+		if <T![Number]>::peek(p, c) {
+			let vector = p.parse::<Vector<'a>>()?;
+			return Ok(Self::NumberAngle(vector, p.parse::<CalcableValue<'a, Angle>>()?));
+		}
+		let angle = p.parse::<CalcableValue<'a, Angle>>()?;
+		let c = p.peek_n(1);
+		if <T![Ident]>::peek(p, c) {
+			let variant = match p.to_atom::<CssAtomSet>(c) {
+				CssAtomSet::X => RotateStyleValue::XAngle,
+				CssAtomSet::Y => RotateStyleValue::YAngle,
+				CssAtomSet::Z => RotateStyleValue::ZAngle,
+				_ => return Ok(Self::Angle(angle)),
+			};
+			let keyword = p.parse::<KeywordValue<'a, T![Ident]>>()?;
+			return Ok(variant(keyword, angle));
+		} else if <T![Number]>::peek(p, c) {
+			return Ok(Self::NumberAngle(p.parse::<Vector<'a>>()?, angle));
+		}
+		Ok(Self::Angle(angle))
 	}
 }
 
@@ -42,6 +85,28 @@ mod tests {
 	fn test_errors() {
 		assert_peek_false!(CssAtomSet::ATOMS, TransformStyleValue, "auto");
 		assert_parse_error!(CssAtomSet::ATOMS, ScaleStyleValue, "none none");
+	}
+
+	#[test]
+	fn test_rotate() {
+		assert_parse!(CssAtomSet::ATOMS, RotateStyleValue, "none");
+		assert_parse!(CssAtomSet::ATOMS, RotateStyleValue, "45deg");
+		assert_parse!(CssAtomSet::ATOMS, RotateStyleValue, "x 45deg");
+		assert_parse!(CssAtomSet::ATOMS, RotateStyleValue, "y 45deg");
+		assert_parse!(CssAtomSet::ATOMS, RotateStyleValue, "z 45deg");
+		assert_parse!(CssAtomSet::ATOMS, RotateStyleValue, "45deg x");
+		assert_parse!(CssAtomSet::ATOMS, RotateStyleValue, "1 2 3 45deg");
+		assert_parse!(CssAtomSet::ATOMS, RotateStyleValue, "45deg 1 2 3");
+		assert_parse!(CssAtomSet::ATOMS, RotateStyleValue, "z calc(45deg * 2)");
+		assert_parse!(CssAtomSet::ATOMS, RotateStyleValue, "x var(--angle)");
+		assert_parse!(CssAtomSet::ATOMS, RotateStyleValue, "var(--angle) y");
+	}
+
+	#[test]
+	fn test_rotate_errors() {
+		assert_parse_error!(CssAtomSet::ATOMS, RotateStyleValue, "x");
+		assert_parse_error!(CssAtomSet::ATOMS, RotateStyleValue, "1 2 45deg");
+		assert_parse_error!(CssAtomSet::ATOMS, RotateStyleValue, "none 45deg");
 	}
 
 	#[test]
