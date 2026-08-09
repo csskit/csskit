46be310dcd12cd41088c8d949e48c92830aa22ca
Keith Cirkel <keithamus@users.noreply.github.com>
2026-08-09 20:01:57 +0100

    css_ast: Implement mask-border style value

--- crates/css_ast/src/values/masking/mod.rs
+++ crates/css_ast/src/values/masking/mod.rs
@@ -126,53 +126,37 @@ pub enum ClipRuleStyleValue<'a> {}
 #[derive(csskit_derives::NodeWithMetadata)]
 pub struct MaskStyleValue<'a>;
 
-// /// Represents the style value for `mask-border` as defined in [css-masking-1](https://drafts.csswg.org/css-masking-1/#mask-border).
-// ///
-// /// The mask-border CSS property sets how the edges of an element are masked. It is a shorthand for mask-border-outset, mask-border-repeat, mask-border-slice, mask-border-source, and mask-border-width.
-// ///
-// /// The grammar is defined as:
-// ///
-// /// ```text,ignore
-// /// <'mask-border-source'> || <'mask-border-slice'> [ / <'mask-border-width'>? [ / <'mask-border-outset'> ]? ]? || <'mask-border-repeat'> || <'mask-border-mode'>
-// /// ```
-// ///
-// /// https://drafts.csswg.org/css-masking-1/#mask-border
-// #[syntax(
-//     " <'mask-border-source'> || <'mask-border-slice'> [ / <'mask-border-width'>? [ / <'mask-border-outset'> ]? ]? || <'mask-border-repeat'> || <'mask-border-mode'> "
-// )]
-// #[derive(
-//     Parse,
-//     Peek,
-//     ToSpan,
-//     ToCursors,
-//     DeclarationMetadata,
-//     SemanticEq,
-//     Debug,
-//     Clone,
-//     PartialEq,
-//     Eq,
-//     PartialOrd,
-//     Ord,
-//     Hash,
-// )]
-// #[declaration_metadata(
-//     initial = "See individual properties",
-//     applies_to = Unknown,
-//     animation_type = Unknown,
-//     longhands = MaskBorderMode|MaskBorderOutset|MaskBorderRepeat|MaskBorderSlice|MaskBorderSource|MaskBorderWidth,
-//     property_group = Masking,
-//     computed_value_type = Unknown,
-//     canonical_order = "per grammar",
-// )]
-// #[cfg_attr(feature = "serde", derive(serde::Serialize), serde())]
-// #[cfg_attr(
-//     feature = "css_feature_data",
-//     derive(ToCSSFeature),
-//     css_feature("css.properties.mask-border")
-// )]
-// #[cfg_attr(feature = "visitable", derive(Visitable), visit)]
-// #[derive(csskit_derives::NodeWithMetadata)]
-// pub struct MaskBorderStyleValue<'a>;
+/// Represents the style value for `mask-border` as defined in [css-masking-1](https://drafts.csswg.org/css-masking-1/#mask-border).
+///
+/// The mask-border CSS property sets how the edges of an element are masked. It is a shorthand for mask-border-outset, mask-border-repeat, mask-border-slice, mask-border-source, and mask-border-width.
+///
+/// The grammar is defined as:
+///
+/// ```text,ignore
+/// <'mask-border-source'> || <'mask-border-slice'> [ / <'mask-border-width'>? [ / <'mask-border-outset'> ]? ]? || <'mask-border-repeat'> || <'mask-border-mode'>
+/// ```
+///
+/// https://drafts.csswg.org/css-masking-1/#mask-border
+#[syntax(
+	" <'mask-border-source'> || <'mask-border-slice'> [ / <'mask-border-width'>? [ / <'mask-border-outset'> ]? ]? || <'mask-border-repeat'> || <'mask-border-mode'> "
+)]
+#[derive(
+	Parse, Peek, ToSpan, ToCursors, DeclarationMetadata, SemanticEq, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash,
+)]
+#[declaration_metadata(
+    initial = "See individual properties",
+    applies_to = Unknown,
+    animation_type = Unknown,
+    longhands = MaskBorderMode|MaskBorderOutset|MaskBorderRepeat|MaskBorderSlice|MaskBorderSource|MaskBorderWidth,
+    property_group = Masking,
+    computed_value_type = Unknown,
+    canonical_order = "per grammar",
+)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize), serde())]
+#[cfg_attr(feature = "css_feature_data", derive(ToCSSFeature), css_feature("css.properties.mask-border"))]
+#[cfg_attr(feature = "visitable", derive(Visitable), visit)]
+#[derive(csskit_derives::NodeWithMetadata)]
+pub struct MaskBorderStyleValue<'a>;
 
 /// Represents the style value for `mask-border-mode` as defined in [css-masking-1](https://drafts.csswg.org/css-masking-1/#mask-border-mode).
 ///
