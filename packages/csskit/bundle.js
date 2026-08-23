406a0a5a1fe324590fdbac11f58d3f76b8b4b19c
Keith Cirkel <keithamus@users.noreply.github.com>
2026-08-23 19:05:00 +0100

    node: Stop doubling wasm builds! (#1447)
    Exploring why the node package is so large I realised we... we have two
    copies of the WASM build.
    
    This removes one of them! Package sizes massively shrunk!

--- packages/csskit/bundle.js
+++ packages/csskit/bundle.js
@@ -3,9 +3,9 @@
 // This entry needs no native addon, thus it runs where wasm runs. It has no object model: `parse`
 // gives a serialised AST and diagnostics. For the object model use the `csskit` entry.
 
-import { parse_error_report } from './wasm.js';
+import { parse_error_report } from './bundle/csskit_wasm_node.js';
 
-export { format, lex, minify, parse } from './wasm.js';
+export { format, lex, minify, parse } from './bundle/csskit_wasm_node.js';
 
 /** Render a human-readable parse-error report for `source`. */
 export function parseErrorReport(source) {
